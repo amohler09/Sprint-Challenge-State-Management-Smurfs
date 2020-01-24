f97bd36ded5150a9e93e875702a7a2aa5f53a242
Amber Mohler <ambermohler@iMac.local>
2020-01-24 11:31:32 -0500

    decided to use Redux, set up store and basic reducer and initialState

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+    isLoading: false,
+    smurfReceived: {},
+    error: '',
+    smurfSent: {}
+};
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
