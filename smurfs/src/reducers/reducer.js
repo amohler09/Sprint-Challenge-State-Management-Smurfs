52d9de4592ed8ca81b07d50b8bec90b3e4fc85d5
Amber Mohler <ambermohler@iMac.local>
2020-02-21 11:22:07 -0500

    decided to use redux, added all dependencies, created store, basic reducer and tested smurflist component

---
+++ smurfs/src/reducers/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    smurfs: [],
+    isFetching: false,
+    isSending: false
+}
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+
+}
